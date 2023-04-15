28759d27401d4d9335cac0f1ca1f6cfdc5fc6f62
Chidi Nwanneka <cnwanneka@gmail.com>
2023-04-15 20:03:49 +0100

    updated homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,20 @@
 function Home() {
     return (
-      <h1>Hi</h1>
+      <div>
+        <h3>
+          Enthusiastic front-end developer, skilled in bootstrap and react.
+        </h3>
+        <br></br>
+        <h3>
+          Knowledgeable in user interface, testing and debugging processes.
+        </h3>
+
+      </div>
+      
+      
+    
+
+      
     );
   }
   
