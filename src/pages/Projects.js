c81fd3fa4d41878c15fc20112908709b4e740557
Chidi Nwanneka <cnwanneka@gmail.com>
2023-04-16 01:12:13 +0100

    added projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,29 @@
-function Projects() {
-    return (
-      <projects className="projects">
-        <h1>Projects</h1>
-        <h2>On That Day
-          <br></br>
-            Actor Search
-        </h2>
-      </projects>
-    );
-  }
+import React from 'react'; 
+import ProjectCards from '../components/Project'
+import projects from '../projects.json'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Wrapper(props) {
+  return <div className="wrapper">{props.children}</div>;
+}
+
+function Projects () {  
+
+  return (
+      <section className="container">
+        <div className="project">
+          <h2 className="top-title">React Portfolio</h2>
+          <hr></hr>
+        </div>
+
+        <Wrapper id="card-data">
+          {projects.map((project) => (
+            <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} topics={project.topics}/>
+          ))}
+        </Wrapper>
+      </section>
   
-  export default Projects
+  );
+}
+
+export default Projects;
